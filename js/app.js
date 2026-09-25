0faaa274490dca7990359e7b5e81ca8852b3e111
shahedempty111 <shahedafridi112@gmail.com>
2026-09-25 23:45:00 +0600

    Section Added: Header & Hero

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 // Javascript code for influencer product app
+
+const menu = document.getElementById('mobile-menu');
+const nav_item = document.getElementById('nav-item');
+
+menu.addEventListener('click',()=>{
+    nav_item.classList.toggle('active');
+})
